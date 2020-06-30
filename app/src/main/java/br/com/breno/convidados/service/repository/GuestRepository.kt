52792a8b2f6499a0eb3d2ce4bda41ffa95077101
breno-do-val <brenorvale@gmail.com>
2020-06-30 08:29:55 -0300

    Singleton created, insertion and update methods added.

--- app/src/main/java/br/com/breno/convidados/service/repository/GuestRepository.kt
+++ app/src/main/java/br/com/breno/convidados/service/repository/GuestRepository.kt
@@ -1,8 +1,40 @@
 package br.com.breno.convidados.service.repository
 
+import android.content.ContentValues
+import android.content.Context
+import br.com.breno.convidados.constants.DataBaseConstants
 import br.com.breno.convidados.service.model.GuestModel
 
-class GuestRepository {
+class GuestRepository private constructor(context: Context) {
+
+    // Created Singleton to avoid database concurrency
+
+    private var mGuestDataBaseHelper: GuestDataBaseHelper = GuestDataBaseHelper(context)
+
+    companion object {
+        private lateinit var repository: GuestRepository
+
+        fun getInstance(context: Context): GuestRepository {
+            if (!::repository.isInitialized) {
+                repository = GuestRepository(context)
+            }
+            return repository
+        }
+    }
+
+    fun save(guest: GuestModel): Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
+
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+            db.insert(DataBaseConstants.GUEST.TABLE_NAME, null, contentValues)
+            true
+        } catch (e : Exception) {
+            false
+        }
+    }
 
     fun getAll(): List<GuestModel> {
         val list: MutableList<GuestModel> = ArrayList()
@@ -19,10 +51,28 @@ class GuestRepository {
         return list
     }
 
-    fun save(guest: GuestModel) {}
 
-    fun update(guest: GuestModel) {}
+    fun update(guest: GuestModel) : Boolean {
+        return try {
+            val db = mGuestDataBaseHelper.writableDatabase
 
-    fun delete(guest: GuestModel) {}
+            val contentValues = ContentValues()
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            contentValues.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, guest.presence)
+
+            //Criteria for update
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            var args = arrayOf(guest.id.toString())
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, contentValues, selection, args)
+            true
+        } catch (e : Exception) {
+            false
+        }
+    }
+
+    fun delete(guest: GuestModel) {
+
+    }
 
 }
--- app/src/main/java/br/com/breno/convidados/viewModel/GuestFormViewModel.kt
+++ app/src/main/java/br/com/breno/convidados/viewModel/GuestFormViewModel.kt
@@ -1,15 +1,17 @@
 package br.com.breno.convidados.viewModel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.breno.convidados.service.model.GuestModel
 import br.com.breno.convidados.service.repository.GuestRepository
 
-class GuestFormViewModel : ViewModel() {
+class GuestFormViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val mGuestRepository : GuestRepository =
-        GuestRepository()
+    private val mContext = application.applicationContext
+    private val mGuestRepository : GuestRepository = GuestRepository.getInstance(mContext)
 
     private var mSaveGuest = MutableLiveData<Boolean>()
     val saveGuest : LiveData<Boolean> = mSaveGuest
